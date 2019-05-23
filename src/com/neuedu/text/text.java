99a5f05a7142d3bcec34f7d12afd5fdb8c0ded45
a1948230823 <1948230823@qq.com>
2019-05-23 19:51:23 +0800

    sad

--- src/com/neuedu/text/text.java
+++ src/com/neuedu/text/text.java
@@ -4,7 +4,7 @@ public class text {
     public static void main(String[] arg) {
         int a[]={1,3,6,7,8,2,9};
         for(int i=a.length-1; i>=0; i--) {
-            System.out.println(a[i] + ",");
+            System.out.println(a[i]);
         }
     }
 }
