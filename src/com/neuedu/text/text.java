cab240752e17b41870b964d9e7d6ea0f1cc6e052
a1948230823 <1948230823@qq.com>
2019-05-23 19:27:29 +0800

    sad

--- src/com/neuedu/text/text.java
+++ src/com/neuedu/text/text.java
@@ -2,7 +2,7 @@ package com.neuedu.text;
 
 public class text {
     public static void main(String[] arg) {
-        int a[]={1,4,6,78,23,9};
+        int a[]={1,3,6,7,8,2,9};
         for(int i=a.length-1; i>=0; i--) {
             System.out.println(a[i] + ",");
         }
