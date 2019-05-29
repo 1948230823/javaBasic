3b4d71474469801f26c02b4678cfc0db47984e5e
a1948230823 <1948230823@qq.com>
2019-05-29 08:08:50 +0800

    sad

---
+++ src/com/neuedu/text/May28.java
@@ -0,0 +1,42 @@
+package com.neuedu.text;
+
+
+public class May28 {
+
+   /* String name;// 姓名
+    double height;// 身高
+    int age;// 年龄
+
+    void sayHello() {
+        System.out.println("hello,my name is  " + name+" "+height+"m"+" "+age+"岁");
+    }*/
+
+/*
+
+    int x;
+    int y;
+
+
+    May28() {
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+    May28(int x,int y) {
+        x=x;
+        y=y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+    void movePoint(int p1,int p2) {
+
+        p1=x+20;
+        p2=y+20;
+        System.out.println("p1的坐标为"+p1+"\n"+"p2的坐标为"+p2);
+
+    }
+    */
+
+
+
+}
---
+++ src/com/neuedu/text/May28i.java
@@ -0,0 +1,57 @@
+package com.neuedu.text;
+
+public class May28i {
+
+
+  /*
+    char color;
+    int model;
+
+    May28i() {
+
+    }
+
+    May28i(char color, int model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    void a() {
+        System.out.println("颜色：" + color + "  型号：" + model);
+    }
+
+    public static void main(String[] args) {
+        May28i a = new May28i('a', 9527);
+        a.a();
+    }
+    */
+
+/*
+    public static void main(String[] args) {
+        May28 zhangsan = new May28();
+        zhangsan.name = "zhangsan";
+        zhangsan.height = 1.73;
+        zhangsan.age = 33;
+
+       May28 lishi = new May28();
+        lishi.name = "lishi";
+        lishi.height = 1.74;
+        lishi.age = 44;
+
+        zhangsan.sayHello();
+        lishi.sayHello();
+
+    }
+    */
+/*
+    public static void main(String[] args) {
+
+
+        May28i p = new May28i();
+        p.x = 20;
+        p.y = 10;
+        p.moveMay28i(p.x, p.y);
+
+    }
+    */
+}
